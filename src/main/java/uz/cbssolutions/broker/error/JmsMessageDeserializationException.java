3b907d075bcf7ce0f20a4731552cedb36751aa91
Israiloff <stardustindark@gmail.com>
2023-07-26 12:59:31 +0500

    added: message deserialization error;

---
+++ src/main/java/uz/cbssolutions/broker/error/JmsMessageDeserializationException.java
@@ -0,0 +1,10 @@
+package uz.cbssolutions.broker.error;
+
+/**
+ * Indicates that an incoming JMS message deserialization process failed.
+ */
+public class JmsMessageDeserializationException extends Exception {
+    public JmsMessageDeserializationException(Throwable cause) {
+        super("Incoming JMS message deserialization error", cause);
+    }
+}
